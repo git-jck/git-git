965cf780021a1dfe1dbd901c083f034d6c3ad722
git-jck <jckajiwala@gmail.com>
2023-11-02 01:51:41 +0530

    css js png

---
+++ git.js
@@ -0,0 +1,7 @@
+document.getElementById("h1").addEventListener("click", function () {
+	alert("Hello World!");
+});
+
+/* function alert () {
+    alert('Hello World!');  // Alerts "Hello world!" to the user when clicked on h1 element in HTML document (see index.
+} */
